c4ebfc7772bfde83005ad29265fe107e58d69536
Vladimir Halme <vhalme@gmail.com>
2024-12-06 16:26:49 +0200

    Debugging changes

--- apps/user/app/payment/complete/page.tsx
+++ apps/user/app/payment/complete/page.tsx
@@ -1,6 +1,5 @@
 import prisma from '@repo/data/PrismaCient'
 import CompletePage from './CompletePage'
-import { getReservationByPaymentRef } from '../actions'
 
 interface SearchParams {
   searchParams: { [key: string]: string }
@@ -9,25 +8,27 @@ interface SearchParams {
 const { STRIPE_PUBLIC_KEY } = process.env
 
 async function getReservation(paymentRef: string) {
+
+  console.log('GET RESERVATION', paymentRef)
   const reservation = await prisma.reservation.findFirst({ 
     where: { paymentRef }
   })
-
+  console.log('RESERVATION', reservation)
   return reservation
 
 }
 
-export default async function PaymentPage({ searchParams }: SearchParams) {
+export default async function Complete({ searchParams }: SearchParams) {
 
 
-  console.log('render CompletePage', searchParams)
+  console.log('COMPLETE PAGE', searchParams, process.env.STRIPE_PUBLIC_KEY, STRIPE_PUBLIC_KEY)
 
-  if (!STRIPE_PUBLIC_KEY) {
+  if (!process.env.STRIPE_PUBLIC_KEY) {
     console.error('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not set')
-    return null
+    return <div>Misconfiguration</div>
   }
 
-  console.log('STRIPE PUBLISHABLE KEY', STRIPE_PUBLIC_KEY)
+  console.log('STRIPE PUBLISHABLE KEY', process.env.STRIPE_PUBLIC_KEY)
   console.log('searchParams', searchParams)
 
   const { payment_intent, payment_intent_client_secret } = searchParams
@@ -35,17 +36,17 @@ export default async function PaymentPage({ searchParams }: SearchParams) {
 
   if (!payment_intent || !payment_intent_client_secret) {
     console.error('payment_intent or payment_intent_client_secret is not set')
-    return null
+    return <div>Misconfiguration 2</div>
   }
 
   const reservation = await getReservation(payment_intent)
 
   if (!reservation) {
     console.error('Reservation not found')
-    return null
+    return <div>Reservation not found</div>
   }
 
   return (
-    <CompletePage stripePublicKey={STRIPE_PUBLIC_KEY} stripeClientSecret={payment_intent_client_secret} reservation={reservation} />
+    <CompletePage stripePublicKey={process.env.STRIPE_PUBLIC_KEY} stripeClientSecret={payment_intent_client_secret} reservation={reservation} />
   )
 }
